4b66c06520b183b8ccc429a9bc6cb364fd28c8bd
liangfeng <golovin@qq.com>
2026-01-11 13:12:45 +0800

    feat(editor): 支持编辑器图片拖拽/粘贴上传及自定义大小
    - 实现编辑器图片拖拽和粘贴自动上传功能
    - 新增上传进度条组件 (UploadProgress) 和上传 Hook
    - 支持 Markdown 图片自定义大小语法 `![alt|size](url)`
    - 更新前台渲染组件以解析并显示指定大小的图片

---
+++ packages/editor/src/components/index.ts
@@ -0,0 +1,5 @@
+export {UploadProgress } from './UploadProgress';
+export type { UploadItem, UploadProgressProps } from './UploadProgress';
+
+export { ImageSizePopover } from './ImageSizePopover';
+export type { ImageSizePopoverProps } from './ImageSizePopover';
